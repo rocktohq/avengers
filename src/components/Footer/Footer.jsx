0e6681990a7e1adccfb117505af9ba06689ad6f1
Saidul Mursalin <itzmonir@gmail.com>
2023-09-14 01:50:19 +0600

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,8 @@
 export default function Footer() {
   return (
     <footer className="py-5 bg-gray-950">
-      <p className="text-center text-white">&copy; Avenger 2023</p>
+      <p className="text-center text-white">&copy; <span className="text-purple-700">A</span>vengers 2023. All right reserved.</p>
+      <p className="text-center text-white"><a href="https://github.com/rocktohq/avengers.git" target="_blank" rel="noreferrer">Source code[Github]</a></p>
     </footer>
   )
 }
